52f8a7cadc215e7fabab1355bb58cb24a7ddb345
teran210c <a242729@uach.mx>
2025-11-10 14:22:25 -0700

    added level mapping to Path component

--- Components/Path.jsx
+++ Components/Path.jsx
@@ -1,5 +1,16 @@
 export default function Path() {
+    const levels = [
+        {id: 1, unlocked: true},
+        {id: 2, unlocked: false}
+    ]
+
     return (
-        <p>Path component</p>
+        <div>
+            <p>Path component</p>
+            {levels.map(level => (
+                <p>{level.id}</p>
+            ))}
+        </div>
+        
     )
 }
