0e8765845a09300eb70155e0d52fe144672f85ad
teran210c <a242729@uach.mx>
2025-11-11 13:45:28 -0700

    feat: created Board component and added it to page.tsx

---
+++ Components/Board.jsx
@@ -0,0 +1,5 @@
+import { Chessboard } from "react-chessboard";
+
+export default function Board() {
+    return <Chessboard />
+}
--- src/app/board/level-1/page.tsx
+++ src/app/board/level-1/page.tsx
@@ -1,7 +1,12 @@
+"use client"
+
+import Board from "../../../../Components/Board"
+
 export default function level1() {
     return (
         <>
             <p>hello from level 1</p>
+            <Board />
         </>
     )
 }
